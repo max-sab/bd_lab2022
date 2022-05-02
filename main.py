969dabc67f3446c531be70f913f5a63f43b92813
zhulkevskijv <zhulkevskijv@gmail.com>
2022-05-02 19:39:44 +0300

    DB connect

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import pymongo
 
 
 def print_hi(name):
@@ -11,6 +12,14 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    client = pymongo.MongoClient(
+        "mongodb+srv://evo:evolutional@evolutional.aweop.mongodb.net/genes?retryWrites=true&w=majority")
+    db = client['genes']
+    collection = db['sequence']
+    print(collection.find_one())
     print_hi('PyCharm')
+
+
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
