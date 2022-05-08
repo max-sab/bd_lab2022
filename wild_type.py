7c2bd83390fcf2dd708f05e6bd124ec0e48a66f5
Maksym Sabadyshyn <max.sabadyshyn@gmail.com>
2022-05-09 02:30:42 +0300

    added rsrs and rcrs polymorphysms

--- wild_type.py
+++ wild_type.py
@@ -19,7 +19,7 @@ def get_wild_type(db: Database[Mapping[str, Any]], regions=None, databases=None,
         db = client['genes']
         sequence = db['sequence']
 
-        res = sequence.aggregate([
+        return list(sequence.aggregate([
             {
                 "$match":
                     {
@@ -129,131 +129,19 @@ def get_wild_type(db: Database[Mapping[str, Any]], regions=None, databases=None,
                         }
                     }
                 }
-            }
-        ])
-        print(list(res))
-
-
-def calculate_general_wild_type():
-    client = pymongo.MongoClient(
-        "mongodb+srv://evo:evolutional@evolutional.aweop.mongodb.net/genes?retryWrites=true&w=majority")
-    db = client['genes']
-    fasta_collection = db['position_fasta']
-    res = fasta_collection.aggregate([
-        {
-            "$lookup": {
-                "from": "fasta",
-                "localField": "fasta_id",
-                "foreignField": "_id",
-                "as": "positions"
-            }
-        },
-        {"$unwind": "$positions"},
-        {
-            "$project": {
-                "letters": {
-                    "$map": {
-                        "input": {
-                            "$range": [
-                                0,
-                                {
-                                    "$strLenCP": "$positions.fasta"
-                                }
-                            ]
-                        },
-                        "as": "position",
-                        "in": {
-                            "position": "$$position",
-                            "letter": {
-                                "$substr": [
-                                    "$positions.fasta",
-                                    "$$position",
-                                    1
-                                ]
-                            }
-                        }
-                    }
-                }
-            }
-        },
-        {
-            "$unwind": "$letters"
-        },
-        {
-            "$project": {
-                "letter": "$letters.letter",
-                "position": "$letters.position",
-                "_id": 0
-            }
-        },
-        {
-            "$match": {"$expr": {"$lt": ["$position", 377]}}
-        },
-        {
-            "$group": {
-                "_id": {
-                    "position": "$position",
-                    "letter": "$letter"
-                },
-                "count": {
-                    "$sum": 1
-                }
-            }
-        },
-        {
-            "$sort": {
-                "count": -1
-            }
-        },
-        {
-            "$group": {
-                "_id": "$_id.position",
-                "letter": {
-                    "$first": "$_id.letter"
-                },
-
             },
-        },
-        {
-            "$sort": {
-                "_id": 1
-            }
-        },
-        {
-            "$group": {
-                "_id": None,
-                "letters": {
-                    "$push": "$letter"
-                }
-            }
-        },
-        {
-            "$project": {
-                "_id": 0,
-                "letters": {
-                    "$reduce": {
-                        "input": "$letters",
-                        "initialValue": "",
-                        "in": {
-                            "$concat": [
-                                "$$value",
-                                "$$this"
-                            ]
-                        }
-                    }
-                }
-            }
-        }
-    ])
-    print(list(res))
-
+        ]))
 
 def wild_type_to_rCRS_poly():
     client = pymongo.MongoClient(
         "mongodb+srv://evo:evolutional@evolutional.aweop.mongodb.net/genes?retryWrites=true&w=majority")
     db = client['genes']
-    fasta_collection = db['position_fasta']
-    fasta_collection.aggregate([
+    sequence = db['sequence']
+
+    wild_type = get_wild_type(db, ["BK"], [])[0]["letters"]
+    print(wild_type)
+    base_sequence = db['base_sequence'].find_one({"name":"EVA"})["fasta"]
+    res = sequence.aggregate([
         {
             "$project": {
                 "_id": 1,
@@ -275,14 +163,14 @@ def wild_type_to_rCRS_poly():
                                         "$strcasecmp": [
                                             {
                                                 "$substr": [
-                                                    "$fasta",
+                                                    wild_type,
                                                     "$$this",
                                                     1
                                                 ]
                                             },
                                             {
                                                 "$substr": [
-                                                    "$fasta2.fasta",
+                                                    base_sequence,
                                                     "$$this",
                                                     1
                                                 ]
@@ -300,9 +188,6 @@ def wild_type_to_rCRS_poly():
         {
             "$group": {
                 "_id": "$length",
-                "count": {
-                    "$count": {}
-                }
             }
         },
         {
@@ -310,8 +195,6 @@ def wild_type_to_rCRS_poly():
                 "_id": 1
             }
         }
-    ]
+    ])
 
-    )
-
-    return ""
+    print(list(res))
