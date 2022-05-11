13e27d1febe2b0ef42c53bbc9a14d8f4821971fc
Maksym Sabadyshyn <max.sabadyshyn@gmail.com>
2022-05-11 13:47:01 +0300

    gene code and parsing fixed

--- main.py
+++ main.py
@@ -4,13 +4,17 @@ from fasta_parser import parse_fasta
 from populate_db import populate_db
 
 def combine_results(genes, fasta):
+    final_genes = []
     for idx, gene in enumerate(genes):
         needed_fasta = fasta[gene["code"]]
-        genes[idx].update(needed_fasta)
+        if len(needed_fasta["fasta"]) >= 312:
+            genes[idx].update(needed_fasta)
+            final_genes.append(genes[idx])
+    return final_genes
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     genes = parse_files()
     fasta = parse_fasta()
-    combine_results(genes, fasta)
-    populate_db(genes)
+    final_res = combine_results(genes, fasta)
+    populate_db(final_res)
